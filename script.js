71d2b3e01b230bdf44993adc4e495d2527b74841
qumrran <108193430+qumrran@users.noreply.github.com>
2023-07-25 20:41:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const numbers = [1, 8, 5, 4, 6, 7, 8, 9];
 
-const result = numbers.reduce((prev, curr) => prev + curr);
-
-console.log(result);
-
-const result2 = numbers.map(a * a);
-
-console.log(result2);
