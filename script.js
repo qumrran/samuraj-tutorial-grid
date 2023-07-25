4e252ecfe30a49c0067052e94312517574b36fb1
qumrran <108193430+qumrran@users.noreply.github.com>
2023-07-25 20:36:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const numbers = [1, 8, 5, 4, 6, 7, 8, 9];
+
+const result = numbers.reduce((prev, curr) => prev + curr);
+
+console.log(result);
+
+const result2 = numbers.map(a * a);
+
+console.log(result2);
